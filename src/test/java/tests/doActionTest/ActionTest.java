a8e683525f485d22dfbfa4a1cdd0fba103c99bbe
eugeneburdukov <eugenebkv@yahoo.com.ua>
2022-11-22 01:56:05 +0200

    adjusted

--- src/test/java/tests/doActionTest/ActionTest.java
+++ src/test/java/tests/doActionTest/ActionTest.java
@@ -5,7 +5,7 @@ import tests.base.BaseTest;
 
 public class ActionTest extends BaseTest {
 
-    @Test
+    @Test(description = "Open Browser")
     public void doCheckState() {
         basePage.open("https://chromedriver.storage.googleapis.com/index.html?path=107.0.5304.62/");
     }
