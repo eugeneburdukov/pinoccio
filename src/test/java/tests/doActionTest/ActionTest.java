5a0e268010b221c8183fce457675c3f0e71f19b1
eugeneburdukov <eugenebkv@yahoo.com.ua>
2022-11-24 02:41:31 +0200

    added assert

--- src/test/java/tests/doActionTest/ActionTest.java
+++ src/test/java/tests/doActionTest/ActionTest.java
@@ -1,5 +1,6 @@
 package tests.doActionTest;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import tests.base.BaseTest;
 
@@ -8,5 +9,6 @@ public class ActionTest extends BaseTest {
     @Test(description = "Open Browser")
     public void doCheckState() {
         basePage.open("https://chromedriver.storage.googleapis.com/index.html?path=107.0.5304.62/");
+        Assert.assertEquals(2,2);
     }
 }
